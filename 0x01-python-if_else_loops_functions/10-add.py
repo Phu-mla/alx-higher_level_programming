72cad07c76f3894561b615b304ed8ccc258a41d4
Phumla Moloi <Phumlah.m@gmail.com>
2023-08-10 20:53:43 -0700

    initial commit

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def add(x, y):
+    reurn x + y
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for ch in range(97, 123):
+    if chr(ch) != 'e' and chr(ch) != 'q':
+        print("{}".format(chr(ch)), end ='')
