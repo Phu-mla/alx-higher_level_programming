53f8f10c2a37eebec6c5dc43ef7607070d8e245b
Phumla Moloi <91374104+Phu-mla@users.noreply.github.com>
2023-08-11 05:43:08 +0200

    Delete 0x00-python-hello_world/0x01-python-if_else_loops_functions directory

--- 0x00-python-hello_world/0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-for ch in range(97, 123):
-    if chr(ch) != 'e' and chr(ch) != 'q':
-        print("{}".format(chr(ch)), end ='')
--- 0x00-python-hello_world/0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,6 +0,0 @@
-def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    last = number % 10
-    print('{:d}'.format(last), end='')
-    return last
